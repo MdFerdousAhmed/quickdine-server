8d8426bfae37802152747a01419d04ab649bcd44
MD FERDOUS AHMED <148644652+MdFerdousAhmed@users.noreply.github.com>
2026-07-15 10:47:37 +0600

    server add

---
+++ server/server.ts
@@ -0,0 +1,38 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from 'express';
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import restaurantRouter from "./routes/restaurantRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
+
+const app = express();
+
+// Connect to MongoDB
+await connectDB()
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+
+const port = process.env.PORT || 8000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+app.use("/api/auth", authRouter)
+app.use("/api/restaurants", restaurantRouter)
+app.use("/api/bookings", bookingRouter)
+
+// global Error Handel
+app.use((err: Error, req: Request, res: Response, next: NextFunction)=>{
+  console.error("Unhandled error:", err);
+  res.status(500).json({
+    message: err.message || "Internal Server Error",
+    stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
+  })
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
